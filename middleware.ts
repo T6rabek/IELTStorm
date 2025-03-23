ee523e8a0cef645e79f5a7a73b5bf62dcb87f66a
Taphaasssa <raufovtorabek10@gmail.com>
2025-03-23 06:29:22 +0500

    Fixed email sending issue

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,12 @@
 export { auth as middleware } from "@/auth";
+
+export const config = {
+  matcher: "/:path*",
+  runtime: "nodejs",
+  unstable_allowDynamic: [
+    // allows a single file
+    "/lib/db",
+    // use a glob to allow anything in the function-bind 3rd party module
+    "/node_modules/mongoose/dist/**",
+  ],
+};
