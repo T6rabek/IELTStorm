c9d58c158cb976263f9bce5f47d3a2652cb125da
Taphaasssa <raufovtorabek10@gmail.com>
2025-03-23 05:41:55 +0500

    Fixed email sending issue

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
 
+const MONGO_URI = process.env.MONGO_URI as string;
+
+if (!MONGO_URI) {
+  throw new Error("MONGO_URI is missing in environment variables");
+}
+
 const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) {
     return;
   }
 
-  await mongoose.connect(process.env.MONGO_URI as string);
+  await mongoose.connect(MONGO_URI);
 };
 
 export default connectDB;
