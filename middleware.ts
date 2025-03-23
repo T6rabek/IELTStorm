661e12f7376d073971e447a72a3d94e608159aa7
Taphaasssa <raufovtorabek10@gmail.com>
2025-03-23 06:24:04 +0500

    Fixed email sending issue

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,1 @@
 export { auth as middleware } from "@/auth";
-
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    "/((?!_next/static|_next/image|favicon.ico).*)",
-  ],
-  runtime: "nodejs",
-};
